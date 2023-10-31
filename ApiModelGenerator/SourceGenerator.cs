1528573196fc18613d6c8c0e0aedfef772df0cfb
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2023-10-31 19:12:20 -0400

    Initial source generator.

---
+++ ApiModelGenerator/SourceGenerator.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Text;
+
+namespace ApiModelGenerator
+{
+    [Generator]
+    public class SourceGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            var provider = context.SyntaxProvider.ForAttributeWithMetadataName("PocAttributes.ModelGenerationAttribute",
+                IsMatch,
+                Transform);
+
+            context.RegisterSourceOutput(provider, Generate);
+        }
+        private static bool IsMatch(SyntaxNode syntaxNode, CancellationToken cancellationToken)
+        {
+            return syntaxNode is ClassDeclarationSyntax or RecordDeclarationSyntax;
+        }
+
+        private static INamedTypeSymbol Transform(GeneratorAttributeSyntaxContext context, CancellationToken cancellationToken)
+        {
+            return (INamedTypeSymbol)context.TargetSymbol;
+        }
+
+        private static void Generate(SourceProductionContext context, INamedTypeSymbol typeSymbol)
+        {
+            var typeNamespace = typeSymbol.ContainingNamespace.ToDisplayString();
+
+            var properties = typeSymbol
+                .GetMembers()
+                .Where(s => s.Kind == SymbolKind.Property)
+                .Cast<IPropertySymbol>()
+                .ToList();
+
+            var propertiesSb = new StringBuilder();
+            foreach (var property in properties)
+            {
+                propertiesSb.AppendLine($$"""        public {{property.Type.Name}} {{property.Name}} { get; set; }""");
+            }
+
+            var classTemplate = $$"""
+                namespace {{typeNamespace}}
+                {
+                    public class {{typeSymbol.Name}}Post
+                    {
+                {{propertiesSb}}
+                    }
+                }
+                """;
+            context.AddSource($"{typeSymbol.Name}_ApiModels.g.cs", classTemplate);
+        }
+    }
+}
---
+++ CodeGenApiModelsPoc/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using PocAttributes;
+
+namespace CodeGenApiModelsPoc.Models
+{
+    [ModelGeneration]
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string MiddleName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+    }
+}
